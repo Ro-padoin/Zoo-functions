78268537343337feb66e7511acc6a92df8e76d19
Rosalia Padoin Oliveira <koxelinho@icloud.com>
2022-05-22 16:52:07 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return species.reduce((totalResidentes, residente) => {
+      const obj = { ...totalResidentes }; 
+      // const obj = Object.assign({}, totalResidentes);
+      obj[residente.name] = residente.residents.length;
+      return obj;
+    }, {});
+
+    // return species.map(specie => ({[specie.name]: specie.residents.length})); nao da certo assim
+  }
+  const { specie, sex } = animal;
+  if (specie !== undefined && sex !== undefined) {
+    const findSpecieName = species.find(({ name }) => name === specie);
+    const filterSpecieSex = findSpecieName.residents.filter((residente) => residente.sex === sex);
+    return filterSpecieSex.length;
+  }
+  if (specie !== undefined) {
+    const specieList = species.find(({ name }) => name === specie)
+    return specieList.residents.length;
+  }
 }
 
 module.exports = countAnimals;
