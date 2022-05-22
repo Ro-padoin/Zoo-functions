b6851fa497c393ebf4cd9ce654c1ac2550bcb961
Rosalia Padoin Oliveira <koxelinho@icloud.com>
2022-05-22 18:57:52 -0300

    requisito 9

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,39 @@
 const data = require('../data/zoo_data');
 
 function getAnimalMap(options) {
-  // seu código aqui
+  const template = {
+    NE: [],
+    NW: [],
+    SE: [],
+    SW: [],
+  }
+  
+  if (!options) {
+    return data.species.reduce((result, {location, name}) => {
+      if(!result[location].includes(name)) {
+        result[location].push(name);
+      }
+      return result;
+    }, {...template});
+  }
+  const {sorted, sex } = options;
+
+  let result = data.species.reduce((result, {location, name, residents}) => {
+    let _residents = [...residents];
+    if (sex) {
+      _residents = _residents.filter(resident => resident.sex === sex);
+    }
+    const residentsNames = _residents.map(resident => resident.name);
+    if (sorted) {
+      residentsNames.sort();
+    }
+    if (residentsNames.length) result[location][name] = residentsNames;
+    return result;
+  }, {...template});
+
+  return result;
 }
+
+// console.log(getAnimalMap({includeNames: true, sex: 'male', sorted: true}));
 
 module.exports = getAnimalMap;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,54 @@
-const data = require('../data/zoo_data');
+const { hours, species } = require('../data/zoo_data');
+
+const animalName = species.map((animal) => animal.name);
+
+const daysOfTheWeek = Object.keys(hours);
+
+function scheduleTheMonday(scheduleTarget) {
+  return {
+    [scheduleTarget]: {
+      officeHour: 'CLOSED',
+      exhibition: 'The zoo will be closed!',
+    },
+  };
+}
+
+function schedulePerAnimal(scheduleTarget) {
+  const individualAgenda = species.find((specie) => specie.name.includes(scheduleTarget));
+  return individualAgenda.availability;
+}
+
+function scheduleByDay(scheduleTarget, { open, close }, exhibition) {
+  return {
+    [scheduleTarget]: {
+      officeHour: `Open from ${open}am until ${close}pm`,
+      exhibition,
+    },
+  };
+}
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (!daysOfTheWeek.includes(scheduleTarget) && !animalName.includes(scheduleTarget)) {
+    return daysOfTheWeek.reduce((entireAgenda, day) => ({
+      ...entireAgenda,
+      ...getSchedule(day),
+    }), {});
+  }
+
+  if (animalName.includes(scheduleTarget)) return schedulePerAnimal(scheduleTarget);
+  
+  const filterExhibition = species.filter(({ availability }) => availability
+    .includes(scheduleTarget));
+
+  const mapExhibition = filterExhibition.map(({ name }) => name);
+
+  const agenda = hours[scheduleTarget];  
+
+  if (scheduleTarget === 'Monday') return scheduleTheMonday(scheduleTarget);
+
+  return scheduleByDay(scheduleTarget, agenda, mapExhibition);
 }
+
+console.log(schedulePerAnimal('penguins'));
 
 module.exports = getSchedule;
