9c194bba7014ae6b0fb5677c011723d01d64103f
Rosalia Padoin Oliveira <koxelinho@icloud.com>
2022-05-22 19:00:10 -0300

    requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
-const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const primeiroAnimalResp = employees.find((numeroId) => numeroId.id === id).responsibleFor[0]; // retorna os ID dos animals que o employee e responsavel.
+  const animal = species.find((especie) => especie.id === primeiroAnimalResp).residents;
+  return animal.reduce((maisVelho, especie) => {
+    if (especie.age > maisVelho[2]) {
+      return [especie.name, especie.sex, especie.age];
+    }
+    return maisVelho;
+  }, ['', '', 0]);
 }
 
 module.exports = getOldestFromFirstSpecies;
