8a98ff1f35840be4c0be4c2f85c39263551bcec6
Rosalia Padoin Oliveira <koxelinho@icloud.com>
2022-05-22 15:20:42 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+const { species } = data
+
+function getSpeciesByIds(...ids) {
+  // return species.filter(specie => ids.includes(specie.id));
+  // return species.filter((specie) => ids.find((id) => id === specie.id));
+
+  return species.reduce((acc, specie) => {
+    ids.forEach((id) => {
+      if (id === specie.id) return acc.push(specie)})
+    return acc;
+  }, [])
 }
 
 module.exports = getSpeciesByIds;
