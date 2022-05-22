74addcf6f1c50ea9ce4fba0c1207724a3a477e05
Rosalia Padoin Oliveira <koxelinho@icloud.com>
2022-05-22 16:27:22 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,25 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const searchaManager = employees
+  .reduce((totalGerente, employee) => totalGerente.concat(...employee.managers), []);
+  // .reduce((totalGerente, employee) => {
+  //   totalGerente.push(...employee.managers)
+  //   return totalGerente;
+  // }, []);
+  return searchaManager.includes(id);
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  if (isManager(managerId)) {
+    const filterManager = employees
+      .filter((employee) => employee.managers.some((manager) => manager === managerId));
+    const result = filterManager.map(({ firstName, lastName }) => `${firstName} ${lastName}`);
+    return result;
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
