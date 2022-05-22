717f5a4fa17e32f92935dbcd409408cd59541649
Rosalia Padoin Oliveira <koxelinho@icloud.com>
2022-05-22 15:48:19 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const findEmployees = employees
+    .find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
+  return findEmployees === undefined ? {} : findEmployees;
+
+  // return employees.reduce((acc, employee) => {
+  //   if (employee.firstName === employeeName || employee.lastName === employeeName) acc = employee;
+  //   return acc;
+  // }, {})
+
 }
 
 module.exports = getEmployeeByName;
