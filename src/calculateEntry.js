7de6fad7d693e2ebe10e77214b14b15b5da64acc
Rosalia Padoin Oliveira <koxelinho@icloud.com>
2022-05-22 17:15:48 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,30 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
+
+const countByAgeGroup = (entrants) => entrants.reduce((populacao, { age }) => {
+  let { adult, child, senior } = populacao;
+  if (age < 50 && age >= 18) adult += 1;
+  if (age < 18) child += 1;
+  if (age >= 50) senior += 1;
+  return { adult, child, senior };
+}, { adult: 0, child: 0, senior: 0 });
 
 function countEntrants(entrants) {
-  // seu código aqui
+  if (entrants === undefined) return 0;
+  return countByAgeGroup(entrants);
 }
 
+const countTotalPrice = (entrants) => entrants.reduce((acc, { age }) => {
+  const { adult, senior, child } = prices;
+  if (age < 18) return Number((acc + child).toFixed(2));
+  if (age >= 50) return Number((acc + senior).toFixed(2));
+  return Number((acc + adult).toFixed(2));
+}, 0);
+
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) return 0;
+
+  const precos = countTotalPrice(entrants);
+  return precos;
 }
 
 module.exports = { calculateEntry, countEntrants };
